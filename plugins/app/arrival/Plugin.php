a00344a249a698b7d72d9928d08566355d741c94
dnomjr <dano.majer@protonmail.com>
2023-04-03 16:19:14 +0200

    first commit

---
+++ plugins/app/arrival/Plugin.php
@@ -0,0 +1,91 @@
+<?php namespace App\Arrival;
+
+use Backend\Facades\Backend;
+use System\Classes\PluginBase;
+
+/**
+ * Arrival Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'Arrival',
+            'description' => 'No description provided yet...',
+            'author'      => 'App',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+
+    /**
+     * Registers any front-end components implemented in this plugin.
+     *
+     * @return array
+     */
+    public function registerComponents()
+    {
+        return [];
+        return [
+            'App\Arrival\Components\MyComponent' => 'myComponent',
+        ];
+    }
+
+    /**
+     * Registers any back-end permissions used by this plugin.
+     *
+     * @return array
+     */
+    public function registerPermissions()
+    {   
+        return [
+            'app.arrival.some_permission' => [
+                'tab' => 'Arrival',
+                'label' => 'Some permission'
+            ],
+        ];
+    }
+
+    /**
+     * Registers back-end navigation items for this plugin.
+     *
+     * @return array
+     */
+    public function registerNavigation()
+    {
+        return [
+            'arrival' => [
+                'label'       => 'Arrival',
+                'url'         => Backend::url('app/arrival/arrivals'),
+                'icon'        => 'icon-leaf',
+                'permissions' => ['app.arrival.*'],
+                'order'       => 500,
+            ],
+        ];
+    }
+    
+    /**
+     * Register method, called when the plugin is first registered.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Boot method, called right before the request route.
+     *
+     * @return array
+     */
+    public function boot()
+    {
+
+    }
+}
---
+++ plugins/app/arrival/updates/create_arrivals_table.php
@@ -0,0 +1,23 @@
+<?php namespace App\Arrival\Updates;
+
+use Illuminate\Support\Facades\Schema;
+use October\Rain\Database\Schema\Blueprint;
+use October\Rain\Database\Updates\Migration;
+
+class CreateArrivalsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('arrivals', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('arrivals');
+    }
+}
