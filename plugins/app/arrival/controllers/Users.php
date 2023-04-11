bb0d929e1bed3f9dce2855c83eb6b02c8bfdabdd
dnomjr <dano.majer@protonmail.com>
2023-04-11 21:26:39 +0200

    api

--- plugins/app/arrival/controllers/Users.php
+++ plugins/app/arrival/controllers/Users.php
@@ -7,14 +7,14 @@ use App\Arrival\Models\Arrival;
 
 class Users extends Controller
 {
-    public function store(Request $request)
+    /*public function store(Request $request)
     {
         $user = new Arrival();
         $user->name = $request->name;
         $user->save();
 
         return response()->json('Succeed!');
-    }
+    }*/
 }
 
 
--- plugins/app/arrival/routes.php
+++ plugins/app/arrival/routes.php
@@ -1,9 +1,20 @@
-<? use App\Arrival\Models\Arrival;
+<? 
+use App\Arrival\Models\Arrival;
+use Illuminate\Http\Request;
 
 Route::get('api', function () {
     return Arrival::get();
 });
 
-Route::post('users', 'App\Arrival\Controllers\Users@store');
+Route::post('users', function (Request $request)
+{
+    $user = new Arrival();
+    $user->name = $request->name;
+    $user->save();
+
+    return response()->json('Succeed!');
+});
+
+
 
 ?>
